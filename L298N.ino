c482a2aec1c78de8332060ddd42cf558f6097ebf
atharvakaplay123 <67966647+atharvakaplay123@users.noreply.github.com>
2025-04-12 09:23:07 +0530

    new function ".Move"

--- L298N.ino
+++ L298N.ino
@@ -11,35 +11,17 @@ void setup() {
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
+  pinMode(EN1, OUTPUT);
+  pinMode(EN2, OUTPUT);
 }
 */
-void Forward() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  //Serial.println("Forward");
-}
-void Backward() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  //Serial.println("Backward");
-}
-void Left() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  //Serial.println("Left");
-}
-void Right() {
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  //Serial.println("Right");
+void Move(bool en1_direction, bool en2_direction, float en1, float en2){
+    digitalWrite(IN1, en1_direction);
+    digitalWrite(IN2, !en1_direction);
+    digitalWrite(IN3, en2_direction);
+    digitalWrite(IN4, !en2_direction);
+    analogWrite(EN1, en1);
+    analogWrite(EN2, en2); 
 }
 
 void Stop() {
@@ -47,5 +29,45 @@ void Stop() {
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, LOW);
+  analogWrite(EN1, 0);
+  analogWrite(EN1, 0);
   //Serial.println("Stop");
 }
+/*
+void Forward(int en1, int en2) {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(EN1, en1);
+  analogWrite(EN1, en2);
+  //Serial.println("Forward");
+}
+void Backward(int en1, int en2) {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(EN1, en1);
+  analogWrite(EN1, en2);
+  //Serial.println("Backward");
+}
+void Left(int en1, int en2) {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(EN1, en1);
+  analogWrite(EN1, en2);
+  //Serial.println("Left");
+}
+void Right(int en1, int en2) {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(EN1, en1);
+  analogWrite(EN1, en2);
+  //Serial.println("Right");
+}
+*/
