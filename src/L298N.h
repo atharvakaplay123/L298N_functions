15d74556f01837bfe242384aa506f0990f7bc0bf
atharvakaplay123 <atharvakaplay@gmail.com>
2025-07-02 19:34:07 +0530

    cleaned the code and created the debug and SetMaxSpeed functions, although SetMaxSpeed is not working so its used direclty for now

--- src/L298N.h
+++ src/L298N.h
@@ -1,19 +1,18 @@
 #ifndef L298N_H
 #define L298N_H
 
-class L298N
-{
-private:
+class L298N {
+ private:
   int IN1, IN2, IN3, IN4, EN1, EN2;
   bool Debug = 0;
-  float MaxSpeed;
+  // float MaxSpeed = 255;
 
-public:
-  L298N(int in1, int in2, int in3, int in4, int en1, int en2, float max = 255, bool debug = 0);
+ public:
+  L298N(int in1, int in2, int in3, int in4, int en1, int en2);
+  // void SetMaxSpeed(int speed);
+  void debug(bool debugging);
   void Move(bool en1_direction, bool en2_direction, float en1_speed = 255, float en2_speed = 255);
   void Stop();
-  // void Move(bool en1_direction, bool en2_direction, float speed = MaxSpeed);
-  // void Move(bool en1_direction, bool en2_direction);
 };
 
 #endif
